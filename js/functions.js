f5ac84cd2920263e0cddfc5a61ccf21e97d5f647
Кристина <kris_al2005@mail.ru>
2025-05-20 19:41:46 +0300

    Добавляет функцию проверки диапазона времени

--- js/functions.js
+++ js/functions.js
@@ -15,3 +15,17 @@ const getNumber = (line = '') => {
 };
 
 getNumber(2023);
+
+const checkTimeRange = (start = '', end = '', meeting = '', duration = 1) => {
+  const startTimeValues = start.split(':');
+  const endTimeValues = end.split(':');
+  const meetingTimeValues = meeting.split(':');
+
+  const startMin = Number(startTimeValues[0]) * 60 + Number(startTimeValues[1]);
+  const endMin = Number(endTimeValues[0]) * 60 + Number(endTimeValues[1]);
+  const meetingMin = Number(meetingTimeValues[0]) * 60 + duration;
+
+  return meetingMin >= startMin && meetingMin <= endMin;
+};
+
+checkTimeRange('08:00', '17:30', '14:00', 90);
