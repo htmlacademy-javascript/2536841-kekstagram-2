49051858ecfaa7d235ae60c650a9907bc8601f25
Кристина <kris_al2005@mail.ru>
2025-05-29 21:45:17 +0300

    Добавляет просмотр фотографий в полноразмерном режиме

--- js/posts.js
+++ js/posts.js
@@ -1,22 +1,33 @@
-import {posts} from './createPostsArray.js';
-
-const POSTS_COUNT = 25;
+import {onPostOpen} from './viewPost.js';
 
 const template = document.querySelector('#picture').content.querySelector('.picture');
 const list = document.querySelector('.pictures');
 const fragment = document.createDocumentFragment();
 
-posts(POSTS_COUNT).forEach(({url, description, likes, comments}) => {
-  const post = template.cloneNode(true);
+const createNewPost = (post) => {
+  const {url, description, likes, comments} = post;
+  const newPost = template.cloneNode(true);
 
-  const image = post.querySelector('.picture__img');
+  const image = newPost.querySelector('.picture__img');
   image.src = url;
   image.alt = description;
 
-  post.querySelector('.picture__likes').textContent = likes;
-  post.querySelector('.picture__comments').textContent = comments.length;
+  newPost.querySelector('.picture__likes').textContent = likes;
+  newPost.querySelector('.picture__comments').textContent = comments.length;
 
-  fragment.append(post);
-});
+  newPost.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    onPostOpen(post);
+  });
 
-list.append(fragment);
+  return newPost;
+};
+
+const renderPosts = (postsArray = []) => {
+  postsArray.forEach((postsArrayItem) => {
+    fragment.append(createNewPost(postsArrayItem));
+  });
+  list.append(fragment);
+};
+
+export {renderPosts};
