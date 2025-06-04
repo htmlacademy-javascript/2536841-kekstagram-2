024c03859ec72ab4fb8f997c947a3f895ccbf355
Кристина <kris_al2005@mail.ru>
2025-06-04 15:24:49 +0300

    Вносит изменения по замечаниям

--- js/form.js
+++ js/form.js
@@ -1,5 +1,8 @@
 import {isEscapeKey, stopEventPropagation} from './utils.js';
 
+const MAX_QUANTITY = 5;
+const MAX_LENGTH = 140;
+
 const body = document.querySelector('body ');
 const loadForm = body.querySelector('.img-upload__form');
 const loadInput = loadForm.querySelector('.img-upload__input');
@@ -13,14 +16,13 @@ const loadItemEffect = loadModal.querySelector('.effects__radio');
 const loadItemHashtags = loadModal.querySelector('.text__hashtags');
 const loadItemDescription = loadModal.querySelector('.text__description');
 
-const hashtagsMaxQuantity = 5;
 const hashtagsFieldRegExp = /^#[a-zа-яё0-9]{1,19}$/i;
-const hashtagsFieldRegExpError = 'Хэштеги должны начинаться с #, быть длиной до 20 символов, разделяться пробелом и состоять из букв или цифр';
-const hashtagsFieldRepeatError = 'Хэштеги не должны повторяться';
-const hashtagsFieldQuantityError = `Не может быть больше ${hashtagsMaxQuantity} хэштегов`;
-
-const descriptionFieldMaxLength = 140;
-const descriptionFieldError = `Длина комментария не может составлять больше ${descriptionFieldMaxLength} символов`;
+const Errors = {
+  REGEXP: 'Хэштеги должны начинаться с #, быть длиной до 20 символов, разделяться пробелом и состоять из букв или цифр',
+  REPEAT: 'Хэштеги не должны повторяться',
+  QUANTITY: `Не может быть больше ${MAX_QUANTITY} хэштегов`,
+  LENGTH: `Длина комментария не может составлять больше ${MAX_LENGTH} символов`,
+};
 
 const onDocumentKeydown = (evt) => {
   if (isEscapeKey(evt)) {
@@ -80,19 +82,19 @@ const validateHashtags = (value) => {
     return true;
   }
 
-  const normalizeValue = value.replace(/\B\s+|\s+\B/g, ' ').trim().toLowerCase();
-  const hashtagsList = normalizeValue.split(' ');
+  const normalizeValue = value.trim().toLowerCase();
+  const hashtagsList = normalizeValue.split(/\s+/);
 
-  if (hashtagsList.length > hashtagsMaxQuantity) {
-    validateHashtagsError = hashtagsFieldQuantityError;
+  if (hashtagsList.length > MAX_QUANTITY) {
+    validateHashtagsError = Errors.QUANTITY;
     return false;
   } else if (hashtagsList.length !== new Set(hashtagsList).size) {
-    validateHashtagsError = hashtagsFieldRepeatError;
+    validateHashtagsError = Errors.REPEAT;
     return false;
   }
   for (let i = 0; i < hashtagsList.length; i++) {
     if (!hashtagsFieldRegExp.test(hashtagsList[i])) {
-      validateHashtagsError = hashtagsFieldRegExpError;
+      validateHashtagsError = Errors.REGEXP;
       return false;
     }
   }
@@ -100,10 +102,10 @@ const validateHashtags = (value) => {
   return true;
 };
 
-const validateDescription = (value) => value.length <= descriptionFieldMaxLength;
+const validateDescription = (value) => value.length <= MAX_LENGTH;
 
 pristine.addValidator(loadItemHashtags, validateHashtags, getHashtagsError);
-pristine.addValidator(loadItemDescription, validateDescription, descriptionFieldError);
+pristine.addValidator(loadItemDescription, validateDescription, Errors.LENGTH);
 
 loadForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
